e7c5a087bdcc0ccc781fa1b1f8f3184c104b90a4
Anna Huang <annatointro@gmail.com>
2019-06-04 13:38:54 -0600

    add login template from bootstrap

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-Vue.config.productionTip = false
-Vue.use(VueAxios, axios)
+Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
