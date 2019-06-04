749098136a31cefc57d4804bc6ecfd867a6ee880
Anna Huang <annatointro@gmail.com>
2019-06-04 16:38:27 -0600

    add dashboard template and add sub-routes in dashboard router-view

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Login from "./views/Login.vue";
+import Dashboard from "./views/Dashboard.vue";
+import Products from "./views/Products.vue";
 
 Vue.use(Router);
 
@@ -23,13 +25,17 @@ export default new Router({
       component: Login
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/admin",
+      name: "dashboard",
+      component: Dashboard,
+      children: [
+        {
+          path: "products",
+          name: "products",
+          component: Products,
+          meta: { requiresAuth: true }
+        }
+      ]
     }
   ]
 });
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <NavBar/>
+    <div class="container-fluid">
+      <div class="row">
+        <SideBar/>
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+          <router-view/>
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SideBar from "../components/SideBar";
+import NavBar from "../components/NavBar";
+
+export default {
+  components: {
+    SideBar,
+    NavBar
+  }
+};
+</script>
+
+<style>
+</style>
