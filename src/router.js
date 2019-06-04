b7bd796ebfad8bc331a683d1b7090a338cba8030
Anna Huang <annatointro@gmail.com>
2019-06-04 15:28:04 -0600

    avoiding user enters the page which not exists via URL path

--- src/router.js
+++ src/router.js
@@ -8,6 +8,10 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
+      path: "*",
+      redirect: "login"
+    },
+    {
       path: "/",
       name: "home",
       component: Home,
