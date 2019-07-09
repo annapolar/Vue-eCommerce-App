e140d4df1adc013ef67da6a632236c874e93b262
Anna Huang <annatointro@gmail.com>
2019-07-09 15:52:01 -0600

    [BackStage] redesign coupon list UI

--- src/_BackStage/views/CouponList.vue
+++ src/_BackStage/views/CouponList.vue
@@ -1,99 +1,112 @@
 <template>
   <div>
-    <Spinner v-if="isLoading"/>
-    <div class="text-right mt-4">
-      <button class="btn btn-primary" @click="openDialog(true)">Add Coupon</button>
+    <Spinner v-if="isLoading" />
+    <h3 class="page-name">Coupon List</h3>
+    <button class="btn-add" @click="openDialog(true)">
+      <ion-icon name="add"></ion-icon>
+    </button>
+    <div class="table-overflow">
+      <table class="table-wrap">
+        <thead>
+          <tr>
+            <th>Coupon Title</th>
+            <th>Coupon Code</th>
+            <th>Discount</th>
+            <th>Due Date</th>
+            <th class="text-center">Active</th>
+            <th class="text-center">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="coupon in coupons" :key="coupon.id">
+            <td>{{coupon.title}}</td>
+            <td>{{coupon.code}}</td>
+            <td>{{coupon.percent}}</td>
+            <td>{{coupon.due_date}}</td>
+            <td>
+              <span v-if="coupon.is_enabled" class="text-center item-status item-active"></span>
+              <span v-else class="text-center item-status item-inactive"></span>
+            </td>
+            <td>
+              <div class="text-center">
+                <button class="action-btn" @click="openDialog(false, coupon)">
+                  <ion-icon name="create"></ion-icon>
+                </button>
+                <button class="action-btn" @click="openDeleteDialog(coupon)">
+                  <ion-icon name="trash"></ion-icon>
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <h4>Coupon List</h4>
-    <table class="table mt-4">
-      <thead>
-        <tr>
-          <td>Coupon Title</td>
-          <td>Coupon Code</td>
-          <td>Discount</td>
-          <td>Due Date</td>
-          <td>Available</td>
-          <td>Actions</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="coupon in coupons" :key="coupon.id">
-          <td>{{coupon.title}}</td>
-          <td>{{coupon.code}}</td>
-          <td>{{coupon.percent}}</td>
-          <td>{{coupon.due_date}}</td>
-          <td>
-            <span v-if="coupon.is_enabled" class="text-success">active</span>
-            <span v-else class="text-muted">inactive</span>
-          </td>
-          <td>
-            <span class="d-inline-flex">
-              <button class="btn btn-outline-primary btn-sm" @click="openDialog(false, coupon)">Edit</button>
-              <button class="btn btn-outline-danger btn-sm" @click="openDeleteDialog(coupon)">Delete</button>
-            </span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+
     <!-- ============== Add & Edit Coupon Dialog ================ -->
-    <Dialog v-if="showModal" v-bind="dialogScheme">
-      <div slot="header">
-        <h3>Add New Coupon</h3>
-      </div>
+    <Dialog
+      v-if="showUpdateModal"
+      v-bind="updateDialogScheme"
+      @closeModal="showUpdateModal = false"
+      @confirmModal="updateCoupon"
+    >
       <div slot="body">
-        <div class="form-group">
-          <label for="title">Coupon Title</label>
-          <input type="text" class="form-control" id="title" v-model="tempCoupon.title">
-        </div>
-        <div class="form-group">
-          <label for="percent">Dicount Percent (%)</label>
-          <input type="text" class="form-control" id="percent" v-model="tempCoupon.percent">
-        </div>
-        <div class="form-group">
-          <label for="due_date">Due Date</label>
-          <input type="text" class="form-control" id="due_date" v-model="tempCoupon.due_date">
-        </div>
-        <div class="form-group">
-          <label for="code">Coupon Code</label>
-          <input type="text" class="form-control" id="code" v-model="tempCoupon.code">
-        </div>
-        <div class="form-group">
-          <div class="form-check">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              v-model="tempCoupon.is_enabled"
-              :true-value="1"
-              :false-value="0"
-              id="is_enabled"
-            >
-            <label class="form-check-label" for="is_enabled">Available</label>
+        <div class="coupon-dialog-wrap">
+          <h3 class="dialog-title">Coupon Detail</h3>
+          <div class="form-section">
+            <div class="form-check">
+              <div class="switch-btn">
+                <label class="switch" for="is_enabled">
+                  <input
+                    type="checkbox"
+                    v-model="tempCoupon.is_enabled"
+                    :true-value="1"
+                    :false-value="0"
+                    id="is_enabled"
+                  />
+                  <span class="slider round"></span>
+                </label>
+                Coupon Available
+              </div>
+            </div>
+          </div>
+          <div class="form-section">
+            <label for="title">Coupon Title</label>
+            <input type="text" class="form-control" id="title" v-model="tempCoupon.title" />
+          </div>
+          <div class="form-section">
+            <label for="percent">Percentage (%)</label>
+            <input type="text" class="form-control" id="percent" v-model="tempCoupon.percent" />
+          </div>
+          <div class="form-section">
+            <label for="code">Coupon Code</label>
+            <input type="text" class="form-control" id="code" v-model="tempCoupon.code" />
           </div>
         </div>
-      </div>
-      <div slot="footer">
-        <button type="button" class="btn btn-outline-secondary" @click="showModal=false">Cancel</button>
-        <button type="button" class="btn btn-primary" @click="updateCoupon">Confirm</button>
       </div>
     </Dialog>
 
     <!-- ============== Delete Dialog ================ -->
-    <Dialog v-if="showDeleteModal">
+    <Dialog
+      v-if="showDeleteModal"
+      v-bind="deleteDialogScheme"
+      @closeModal="showDeleteModal = false"
+      @confirmModal="deleteCoupon"
+    >
       <div slot="body">
-        Are you sure you want to delete this coupon ?
-      </div>
-      <div slot="footer">
-        <button @click="showDeleteModal = false">No, cancle</button>
-        <button @click="deleteCoupon()">Yes, delete</button>
+        <div class="dialog-body">
+          You are going to delete coupon:
+          <mark>{{deleteCouponName}}</mark>
+        </div>
       </div>
     </Dialog>
 
     <!-- ============== Pagination ================ -->
-    <Pagination 
-    v-bind="pagination"
-    @getPage="getCoupons" 
-    @getPrev="getCoupons(pagination.current_page - 1)" 
-    @getNext="getCoupons(pagination.current_page + 1)"/>
+    <Pagination
+      v-bind="pagination"
+      @getPage="getCoupons"
+      @getPrev="getCoupons(pagination.current_page - 1)"
+      @getNext="getCoupons(pagination.current_page + 1)"
+    />
   </div>
 </template>
 
@@ -104,13 +117,19 @@ export default {
       coupons: [],
       pagination: {},
       isLoading: false,
-      showModal: false,
+      showUpdateModal: false,
       showDeleteModal: false,
       tempCoupon: {},
-      deleteCouponId:'',
+      deleteCouponId: "",
+      deleteCouponName: "",
       isNew: false,
-      dialogScheme: {
+      updateDialogScheme: {
         maxWidth: 700
+      },
+      deleteDialogScheme: {
+        maxWidth: 500,
+        cancelText: "no, cancel",
+        confirmText: "yes, delete"
       }
     };
   },
@@ -132,42 +151,43 @@ export default {
         this.tempCoupon = Object.assign({}, data);
         this.isNew = false;
       }
-      this.showModal = true;
+      this.showUpdateModal = true;
     },
-    updateCoupon(){
-        let api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_USER}/admin/coupon`;
-        let httpMethod = 'post'
+    updateCoupon() {
+      let api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_USER}/admin/coupon`;
+      let httpMethod = "post";
 
-        if(!this.isNew){
-            api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_USER}/admin/coupon/${this.tempCoupon.id}`;
-            httpMethod = 'put'
+      if (!this.isNew) {
+        api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_USER}/admin/coupon/${this.tempCoupon.id}`;
+        httpMethod = "put";
+      }
+
+      this.$http[httpMethod](api, { data: this.tempCoupon }).then(res => {
+        if ((res.data.success = true)) {
+          this.showUpdateModal = false;
+          this.getCoupons();
+        } else {
+          this.showUpdateModal = false;
+          alert("fail updating coupon, please try again");
         }
-
-       this.$http[httpMethod](api, {data:this.tempCoupon}).then( res => {
-           if(res.data.success = true){
-               this.showModal = false
-               this.getCoupons()
-           }else{
-               this.showModal = false
-               alert('fail updating coupon, please try again')
-           }
-       })
+      });
     },
-    openDeleteDialog(data){
-        this.deleteCouponId = data.id
-        this.showDeleteModal = true
+    openDeleteDialog(data) {
+      this.deleteCouponId = data.id;
+      this.deleteCouponName = data.title;
+      this.showDeleteModal = true;
     },
-    deleteCoupon(){
-        const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_USER}/admin/coupon/${this.deleteCouponId}`;
-        this.$http.delete(api, this.deleteCouponId).then(res => {
-            if(res.data.success = true){
-                this.coupons.splice(this.deleteCouponId, 1)              
-                this.$bus.$emit("pushMesssage", res.data.message, "success")
-            }else{
-                this.$bus.$emit("pushMesssage", res.data.message, "danger")
-            }
-            this.showDeleteModal = false
-        })
+    deleteCoupon() {
+      const api = `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_USER}/admin/coupon/${this.deleteCouponId}`;
+      this.$http.delete(api, this.deleteCouponId).then(res => {
+        if ((res.data.success = true)) {
+          this.coupons.splice(this.deleteCouponId, 1);
+          this.$bus.$emit("pushMesssage", res.data.message, "success");
+        } else {
+          this.$bus.$emit("pushMesssage", res.data.message, "danger");
+        }
+        this.showDeleteModal = false;
+      });
     }
   },
   created() {
@@ -176,5 +196,15 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.coupon-dialog-wrap {
+  .dialog-title {
+    text-align: center;
+    font-family: "Nunito Sans", sans-serif;
+    margin-bottom: 20px;
+  }
+}
+.dialog-body {
+  text-align: center;
+}
 </style>
